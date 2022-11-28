ee83f2b9d96ab68866cf5e20edafb845a3e7e417
Dka-11 <adikaahmad05@gmail.com>
2022-11-28 18:54:06 +0700

    Edit NetworkImage Transform

--- lib/widgets/2.6.1.g_image_transform_sample.dart
+++ lib/widgets/2.6.1.g_image_transform_sample.dart
@@ -10,7 +10,7 @@ class ImageTransformSample extends StatelessWidget {
           body: Container(
             decoration: BoxDecoration(
               image: const DecorationImage(
-                image: NetworkImage('https://unsplash.com/photos/fIq0tET6llw'),
+                image: NetworkImage('https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80'),
                 fit: BoxFit.fitHeight,
               ),
               border: Border.all(
